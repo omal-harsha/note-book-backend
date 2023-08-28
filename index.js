5f8e17205c18c312291c4684e2c78435e1065b6b
Omal Harsha <omalharshanuwan@gmail.com>
2023-08-28 11:20:45 +0530

    create a db connection

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import mongoose from "mongoose";
 
 
 const app = express();
 
-
+mongoose.connect("mongodb+srv://omalharshanuwan:omalmongo@notebook.ljcyzte.mongodb.net/notebook?retryWrites=true&w=majority")
 
 app.listen(3001, ()=> {
     console.log("port 3001 connected")
