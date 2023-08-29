c44df9c61bda7f7567a371396eb999cf5c0a03a1
Omal Harsha <omalharshanuwan@gmail.com>
2023-08-29 12:12:12 +0530

    secure the API
    create the middleware

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,30 @@
 import express from "express";
 const router = express.Router()
+import jwt  from 'jsonwebtoken';
 import {createNote,getNote,deleteNote,updateNote} from '../controllers/noteController.js'
 
+
+//middleware to verify the token in cookies
+const verifyJWT = (req,res,next) => {
+    const token = req.cookies.jwt;
+    if(!token){
+        res.json({message : "Unauthorised"})
+    }else{
+        jwt.verify(token, "secret", (err) => {
+            if(err){
+                res.json({auth: false, message: "invalid token"})
+            }else{
+                next()
+            }
+        })
+    }
+    
+}
+
 //load the note controller
-router.post('/createnote', createNote)
-router.get('/getnotes', getNote)
-router.delete('/deletenote', deleteNote)
-router.patch('/updatenote', updateNote)
+router.post('/createnote',verifyJWT, createNote)
+router.get('/getnotes',verifyJWT, getNote)
+router.delete('/deletenote',verifyJWT, deleteNote)
+router.patch('/updatenote',verifyJWT, updateNote)
 
 export {router};
